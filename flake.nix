59421609d3fddd99281597d1e679fae1fe9b3925
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-07 16:40:40 +0200

    nix: move package to nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,37 +1,17 @@
 {
   description = "corbaserver to provide affordance utilities in python";
 
-  nixConfig = {
-    extra-substituters = [ "https://gepetto.cachix.org" ];
-    extra-trusted-public-keys = [ "gepetto.cachix.org-1:toswMl31VewC0jGkN6+gOelO2Yom0SOHzPwJMY2XiDY=" ];
-  };
-
   inputs = {
-    nixpkgs.url = "github:nim65s/nixpkgs/gepetto";
+    nixpkgs.url = "github:gepetto/nixpkgs/hpp";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    hpp-affordance = {
-      url = "github:humanoid-path-planner/hpp-affordance";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-      };
-    };
-    hpp-corbaserver = {
-      url = "github:humanoid-path-planner/hpp-corbaserver";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-      };
-    };
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ];
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -39,21 +19,27 @@
         "x86_64-darwin"
       ];
       perSystem =
+        { pkgs, self', ... }:
         {
-          self',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          packages = {
-            inherit (pkgs) cachix;
-            default = pkgs.callPackage ./. {
-              hpp-affordance = inputs.hpp-affordance.packages.${system}.default;
-              hpp-corbaserver = inputs.hpp-corbaserver.packages.${system}.default;
-            };
-          };
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          packages = {
+            default = self'.packages.hpp-affordance-corba;
+            hpp-affordance-corba = pkgs.python3Packages.hpp-affordance-corba.overrideAttrs (_: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./data
+                  ./doc
+                  ./idl
+                  ./include
+                  ./package.xml
+                  ./src
+                  ./tests
+                ];
+              };
+            });
+          };
         };
     };
 }
